e82467b8c5d5d73396ed2118e24e8693524ef34b
Carlos <dschworld@gmail.com>
2025-04-10 22:28:06 -0400

    AdminSideBar fixed ;i hope;

--- frontend/src/components/admin/layout/AdminLayout.jsx
+++ frontend/src/components/admin/layout/AdminLayout.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import AdminSidebar from './AdminSidebar';
+import AdminSidebar from './AdminSideBar';
 import AdminHeader from './AdminHeader';
 import { getCurrentUser } from '../../../services/auth.service';
 
